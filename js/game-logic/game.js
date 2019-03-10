642f9004b06049357a96b47bf26ef2d3197bb068
Keith Morton <keefm_6776@yahoo.com>
2019-03-10 17:02:48 +0000

    load another array with reverse sequence

--- js/game-logic/game.js
+++ js/game-logic/game.js
@@ -1,12 +1,22 @@
 var $noOfRounds = 30;
 var $buttons = 4;
 
+function playSequence(arrayToPlay) {
+
+}
+
+
 function startGame() {
   var noToLoad = 0;
   var fwdPlayArray = {};
+  var revPlayArray = {};
+  var j = 0;
   for (var i = $noOfRounds; i--;) {
     noToLoad = Math.trunc(Math.random() * ($buttons - 0.001));
     fwdPlayArray[i] = noToLoad;
-    console.log(noToLoad);
+    console.log(fwdPlayArray[i]+'fwd'+i);
+    revPlayArray[j] = noToLoad;
+    console.log(revPlayArray[j]+'rev'+j);
+    j++;
   }
 }
