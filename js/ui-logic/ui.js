9edaeebcb1fe04d72115cbb92b89f92dc94a43b5
Keith Morton <keefm_6776@yahoo.com>
2019-03-18 20:48:14 +0000

    show/hide buttons for landscape or portrait format

---
+++ js/ui-logic/ui.js
@@ -0,0 +1,23 @@
+	//// When page loads, resizes or changes orientation
+	///  Show/Hide Buttons to suit the screen width/Height ie Landscape and Portrait
+
+	$(window).on('orientation resize load', function() {
+	    var height = $(window).height();
+	    var width = $(window).width();
+	    if (width > height) {
+	        console.log("landscape" + $(this).width());
+	        $('#purple-button-portrait').removeClass('purple-button-style');
+	        $('#brown-button-portrait').removeClass('brown-button-style');
+	        $('#purple-button-landscape').addClass('purple-button-style');
+	        $('#brown-button-landscape').addClass('brown-button-style');
+
+	    }
+	    else {
+	        console.log("portrait" + $(this).width());
+	        $('#purple-button-portrait').addClass('purple-button-style');
+	        $('#brown-button-portrait').addClass('brown-button-style');
+	        $('#purple-button-landscape').removeClass('purple-button-style');
+	        $('#brown-button-landscape').removeClass('brown-button-style');
+	    }
+	});
+	
