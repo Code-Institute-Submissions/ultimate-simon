c0c73813d73d77ad5ff2516742b863e1abf289a7
Keith Morton <keefm_6776@yahoo.com>
2019-03-24 22:28:15 +0000

    control gameParameters with universal object, control button numbers with js

--- js/game-logic/game.js
+++ js/game-logic/game.js
@@ -1,30 +1,29 @@
-
 function playButton(buttonNo) {
   const synth = new Tone.Synth();
   synth.toMaster();
   if (buttonNo == 0) {
-    synth.triggerAttackRelease('A4','8n');
+    synth.triggerAttackRelease('A4', 1.8);
     $('#red-button').animate({ opacity: '1.0' }, "slow").delay('1000').animate({ opacity: '0.5' }, "slow");
   }
   else if (buttonNo == 1) {
-    synth.triggerAttackRelease('E5','8n');
+    synth.triggerAttackRelease('E5', 1.8);
     $('#green-button').animate({ opacity: '1.0' }, "slow").delay('1000').animate({ opacity: '0.5' }, "slow");
   }
   else if (buttonNo == 2) {
-    synth.triggerAttackRelease('C4','8n');
+    synth.triggerAttackRelease('C4', 1.8);
     $('#yellow-button').animate({ opacity: '1.0' }, "slow").delay('1000').animate({ opacity: '0.5' }, "slow");
   }
   else if (buttonNo == 3) {
-    synth.triggerAttackRelease('E4','8n');
+    synth.triggerAttackRelease('E4', 1.8);
     $('#blue-button').animate({ opacity: '1.0' }, "slow").delay('1000').animate({ opacity: '0.5' }, "slow");
   }
   else if (buttonNo == 4) {
-    synth.triggerAttackRelease('C5','8n');
+    synth.triggerAttackRelease('C5', 1.8);
     $('#purple-button-landscape').animate({ opacity: '1.0' }, "slow").delay('1000').animate({ opacity: '0.5' }, "slow");
     $('#purple-button-portrait').animate({ opacity: '1.0' }, "slow").delay('1000').animate({ opacity: '0.5' }, "slow");
   }
   else if (buttonNo == 5) {
-    synth.triggerAttackRelease('A5','8n');
+    synth.triggerAttackRelease('A5', 1.8);
     $('#brown-button-landscape').animate({ opacity: '1.0' }, "slow").delay('1000').animate({ opacity: '0.5' }, "slow");
     $('#brown-button-portrait').animate({ opacity: '1.0' }, "slow").delay('1000').animate({ opacity: '0.5' }, "slow");
   }
@@ -41,18 +40,24 @@ function playSequence(arrayToPlay) {
 
 function startGame() {
   var noToLoad = 0;
-  var fwdPlayArray = {};
-  var revPlayArray = {};
+  //var fwdPlayArray = {};
+  //var revPlayArray = {};
+  var fwdPlayArray = [0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5];
+  var revPlayArray = [5, 4, 3, 2, 1, 0, 5, 4, 3, 2, 1, 0];
   var j = 0;
 
-  for (var i = gameParameters.noOfRounds; i--;) {
+  /*for (var i = gameParameters.noOfRounds; i--;) {
     noToLoad = Math.trunc(Math.random() * (gameParameters.noOfButtons - 0.001));
     fwdPlayArray[i] = noToLoad;
     console.log(fwdPlayArray[i] + 'fwd' + i);
     revPlayArray[j] = noToLoad;
     console.log(revPlayArray[j] + 'rev' + j);
     j++;
+  }*/
+  if (gameParameters.playForward == true) {
+    playSequence(fwdPlayArray);
   }
-  playSequence(fwdPlayArray);
-
+  else {
+    playSequence(revPlayArray);
+  }
 }
--- js/ui-logic/ui.js
+++ js/ui-logic/ui.js
@@ -1,32 +1,42 @@
-	//// When page loads, resizes or changes orientation
-	///  Show/Hide Buttons to suit the screen width/Height ie Landscape and Portrait
-	///  And show either 4 or 6 buttons as per options
+var gameParameters = {
+	currentRound: 1,
+	noOfRounds: 12,
+	noOfButtons: 4,
+	playForward: false
+};
 
-	$(window).on('orientation resize load', function() {
-		var height = $(window).height();
-		var width = $(window).width();
-		if (gameParameters.noOfButtons == 6) {
-			if (width > height) {
-				$('#purple-button-portrait').removeClass('purple-button-style');
-				$('#brown-button-portrait').removeClass('brown-button-style');
-				$('#purple-button-landscape').addClass('purple-button-style');
-				$('#brown-button-landscape').addClass('brown-button-style');
+//// When page loads, resizes or changes orientation
+///  Show/Hide Buttons to suit the screen width/Height ie Landscape and Portrait
+///  And show either 4 or 6 buttons as per options, also adjust button size to suit
+///  The number of buttons on screen
 
-			}
-			else {
-				$('#purple-button-portrait').addClass('purple-button-style');
-				$('#brown-button-portrait').addClass('brown-button-style');
-				$('#purple-button-landscape').removeClass('purple-button-style');
-				$('#brown-button-landscape').removeClass('brown-button-style');
-			}
+$(window).on('orientation resize load', function() {
+	var height = $(window).height();
+	var width = $(window).width();
+	if (gameParameters.noOfButtons == 6) {
+		if (width > height) {
+			$('#purple-button-portrait').removeClass('purple-button-style');
+			$('#brown-button-portrait').removeClass('brown-button-style');
+			$('#purple-button-landscape').addClass('purple-button-style').css({'height': '15vh', 'width': '15vh'});
+			$('#brown-button-landscape').addClass('brown-button-style').css({'height': '15vh', 'width': '15vh'});
 
 		}
 		else {
-			$('#purple-button-portrait').removeClass('purple-button-style');
-			$('#brown-button-portrait').removeClass('brown-button-style');
+			$('#purple-button-portrait').addClass('purple-button-style').css({'height': '15vh', 'width': '15vh'});
+			$('#brown-button-portrait').addClass('brown-button-style').css({'height': '15vh', 'width': '15vh'});
 			$('#purple-button-landscape').removeClass('purple-button-style');
 			$('#brown-button-landscape').removeClass('brown-button-style');
-
 		}
-	});
-	
+
+	}
+	else {
+		$('#purple-button-portrait').removeClass('purple-button-style');
+		$('#brown-button-portrait').removeClass('brown-button-style');
+		$('#purple-button-landscape').removeClass('purple-button-style');
+		$('#brown-button-landscape').removeClass('brown-button-style');
+		$('#red-button').css({'height': '20vh', 'width': '20vh'});
+		$('#yellow-button').css({'height': '20vh', 'width': '20vh'});
+		$('#green-button').css({'height': '20vh', 'width': '20vh'});
+		$('#blue-button').css({'height': '20vh', 'width': '20vh'});
+	}
+});
