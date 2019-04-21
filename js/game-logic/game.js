f7c4d4109238e9011b7213a53ec229d1c031a862
Keith Morton <keefm_6776@yahoo.com>
2019-04-21 21:09:18 +0000

    tweak for HTML, CSS & JS Check.  Comment code fully

--- js/game-logic/game.js
+++ js/game-logic/game.js
@@ -1,94 +1,87 @@
 function resetGame() {
-  fwdPlayArray = [];
-  location.reload();
+     fwdPlayArray = [];        // Empty array of button presses
+     window.location.reload(); // Reload Window To Reset Game
 }
 
 function playButton(buttonNo, delay) { // takes button number from original array or from user click to illuminate the corresponding button and play the tone.
-  const synth = new Tone.Synth();
-  synth.toMaster();
-  if (buttonNo == 0) {
-    synth.triggerAttackRelease('A4', 1.8);
-    $('#red-button').animate({ opacity: '1.0' }, "slow").delay(delay).animate({ opacity: '0.5' }, "slow");
-  }
-  else if (buttonNo == 1) {
-    synth.triggerAttackRelease('E5', 1.8);
-    $('#green-button').animate({ opacity: '1.0' }, "slow").delay(delay).animate({ opacity: '0.5' }, "slow");
-  }
-  else if (buttonNo == 2) {
-    synth.triggerAttackRelease('C4', 1.8);
-    $('#yellow-button').animate({ opacity: '1.0' }, "slow").delay(delay).animate({ opacity: '0.5' }, "slow");
-  }
-  else if (buttonNo == 3) {
-    synth.triggerAttackRelease('E4', 1.8);
-    $('#blue-button').animate({ opacity: '1.0' }, "slow").delay(delay).animate({ opacity: '0.5' }, "slow");
-  }
-  else if (buttonNo == 4) {
-    synth.triggerAttackRelease('C5', 1.8);
-    $('#purple-button-landscape').animate({ opacity: '1.0' }, "slow").delay(delay).animate({ opacity: '0.5' }, "slow");
-    $('#purple-button-portrait').animate({ opacity: '1.0' }, "slow").delay(delay).animate({ opacity: '0.5' }, "slow");
-  }
-  else if (buttonNo == 5) {
-    synth.triggerAttackRelease('A5', 1.8);
-    $('#brown-button-landscape').animate({ opacity: '1.0' }, "slow").delay(delay).animate({ opacity: '0.5' }, "slow");
-    $('#brown-button-portrait').animate({ opacity: '1.0' }, "slow").delay(delay).animate({ opacity: '0.5' }, "slow");
-  }
+     "use strict";
+     const synth = new Tone.Synth();  // start new Tone instatnce
+     synth.toMaster();                // ouput tone to speakers
+     if (buttonNo == 0) {             // if red button
+          synth.triggerAttackRelease("A4", 1.8);  // play red tone
+          $("#red-button").animate({ opacity: "1.0" }, "slow").delay(delay).animate({ opacity: "0.5" }, "slow");  // highlight red button
+     }
+     else if (buttonNo == 1) {        // if green button
+          synth.triggerAttackRelease("E5", 1.8);  // play green tone
+          $("#green-button").animate({ opacity: "1.0" }, "slow").delay(delay).animate({ opacity: "0.5" }, "slow");  // highlight green button
+     }
+     else if (buttonNo == 2) {        // if yellow button
+          synth.triggerAttackRelease("C4", 1.8);  // play yellow tone
+          $("#yellow-button").animate({ opacity: "1.0" }, "slow").delay(delay).animate({ opacity: "0.5" }, "slow");   // highlight yellow button
+     }
+     else if (buttonNo == 3) {        //if blue button
+          synth.triggerAttackRelease("E4", 1.8);  // play blue tone
+          $("#blue-button").animate({ opacity: "1.0" }, "slow").delay(delay).animate({ opacity: "0.5" }, "slow");    // highlight blue button
+     }
+     else if (buttonNo == 4) {        // if purple button
+          synth.triggerAttackRelease("C5", 1.8);  // play purple tone
+          $("#purple-button-landscape").animate({ opacity: "1.0" }, "slow").delay(delay).animate({ opacity: "0.5" }, "slow");  //highlight landscape purple button
+          $("#purple-button-portrait").animate({ opacity: "1.0" }, "slow").delay(delay).animate({ opacity: "0.5" }, "slow");   //highlight portrait purple button
+     }
+     else if (buttonNo == 5) {        // if brown button
+          synth.triggerAttackRelease("A5", 1.8);   // play brown tone
+          $("#brown-button-landscape").animate({ opacity: "1.0" }, "slow").delay(delay).animate({ opacity: "0.5" }, "slow");   // highlight landscape brown button
+          $("#brown-button-portrait").animate({ opacity: "1.0" }, "slow").delay(delay).animate({ opacity: "0.5" }, "slow");    // highlight portrait brown button
+     }
 }
 
 function checkUserInput(arrayToCheck, userArray, userResponse) {
-  if (gameParameters.playForward == true) {
-    SequenceCheck = gameParameters.currentSequenceIndex;
+  "use strict";
+  if (gameParameters.playForward == true) {                         // if forward play selected
+    var SequenceCheck = gameParameters.currentSequenceIndex;        // Forward sequence check used
   }
   else {
-    SequenceCheck = gameParameters.currentSequenceIndexRev;
+    var SequenceCheck = gameParameters.currentSequenceIndexRev;     // if reverse play, revers play used
   }
 
-  console.log("Current Sequence No " + SequenceCheck);
-  console.log("Current Index " + gameParameters.currentSequenceIndex);
-  console.log("Current Rev Index " + gameParameters.currentSequenceIndexRev);
-  
-
-  if (arrayToCheck[SequenceCheck] !== userResponse) {
-    showUserPrompt("Sorry - You Lost!");
-    loserLoser(resetGame, gameParameters.noOfButtons);
+  if (arrayToCheck[SequenceCheck] !== userResponse) {               // if user response is incorrect
+    showUserPrompt("Sorry - You Lost!");                            // display losing message
+    loserLoser(resetGame, gameParameters.noOfButtons);              //play losing button sequence/tones
   }
 
-  if (userArray.length === gameParameters.noOfRounds) {
-    showUserPrompt("Congratulations - You WON!!");
-    winnerWinnerChickenDinner(resetGame, gameParameters.noOfButtons);
+  if (userArray.length === gameParameters.noOfRounds) {             // if user answers all correct
+    showUserPrompt("Congratulations - You WON!!");                  //  display winning message
+    winnerWinnerChickenDinner(resetGame, gameParameters.noOfButtons);  // play winning button sequence/tones
   }
 
-  console.log("currentRound: " + gameParameters.currentRound);
-  console.log("arrayLength: " + userArray.length);
-
-  if (userArray.length === gameParameters.currentRound) {
-    gameParameters.currentSequenceIndex = 0;
-    gameParameters.currentSequenceIndexRev = gameParameters.currentRound;
+  if (userArray.length === gameParameters.currentRound) {           // if all correct in current round
+    gameParameters.currentSequenceIndex = 0;                        // reset index to 0 for forward play
+    gameParameters.currentSequenceIndexRev = gameParameters.currentRound;  // reset index to current round for reverse play
   }
   else {
-    gameParameters.currentSequenceIndex += 1;
-    gameParameters.currentSequenceIndexRev = gameParameters.currentSequenceIndexRev - 1;
+    gameParameters.currentSequenceIndex += 1;                       //  if sequence still being followed move to next respose check fwd
+    gameParameters.currentSequenceIndexRev = gameParameters.currentSequenceIndexRev - 1;  //  if sequence still being followed move to next respose check rev
   }
 
 }
 
 function userSequence(arrayInUse, cbPlaySequence) {
-  $(".simon-button").click(function() { //When user clicks a button div enters function to read response
-    if (gameParameters.isWaitingForUser == false) {
-      showUserPrompt("Watch - Don't Click");
+  "use strict";
+  $(".simon-button").click(function() {               //When user clicks a button div enters function to read response
+    if (gameParameters.isWaitingForUser == false) {   // If user clicks when sequence is played
+      showUserPrompt("Watch - Don't Click");          // ifnore click and prompt don't click
     }
     else {
-      var divNumber = ($(this).data("myval"));
-      playButton(divNumber, '1000');
-      // calls the function to light the button and play the sound
-      userResponseArray.push(divNumber);
-      //stores this clicked button div number in an array to check response against sequence later
-      checkUserInput(arrayInUse, userResponseArray, divNumber);
-      if (userResponseArray.length == gameParameters.currentRound) {
-        gameParameters.currentRound += 1;
-        userResponseArray = [];
-        setTimeout(cbPlaySequence, 3000, arrayInUse);
-        setTimeout(showUserPrompt, 3000, "Watch");
-        setTimeout(showUserInfo, 3000, gameParameters.currentRound);
+      var divNumber = ($(this).data("myval"));        // if user clicks when prompted read button number clicked
+      playButton(divNumber, "1000");                  // highlight clicked button and its tone
+      userResponseArray.push(divNumber);              //stores this clicked button div number in an array to check response against sequence later
+      checkUserInput(arrayInUse, userResponseArray, divNumber);  // check user input clicked against array of correct answers.  userResponse Array passed to assess if round finished
+      if (userResponseArray.length == gameParameters.currentRound) {  //if user has reached end of round
+        gameParameters.currentRound += 1;                             // increase current round by 1
+        userResponseArray = [];                                       // clear user response array
+        setTimeout(cbPlaySequence, 3000, arrayInUse);                 // play sequence of next round
+        setTimeout(showUserPrompt, 3000, "Watch");                    // prompt user to watch the sequence
+        setTimeout(showUserInfo, 3000, gameParameters.currentRound);  // update current round to match round played
       } /*if (userResponseArray.length == gameParameters.currentRound)*/
     } /*if (gameParameters.isWaitingForUser */
   }); /* .click(function() */
@@ -96,37 +89,42 @@ function userSequence(arrayInUse, cbPlaySequence) {
 } /* userSequence function */
 
 function playSequence(arrayToPlay, cbUserSequence, cbplaySequence) {
-  showUserPrompt("Watch")
-  gameParameters.isWaitingForUser = false;
-  for (var k = 0; k < gameParameters.currentRound; k++) {
+  "use strict";
+  showUserPrompt("Watch");                                            // prompt user to watch sequence to copy
+  gameParameters.isWaitingForUser = false;                            // prevent user from clicking
+  for (var k = 0; k < gameParameters.currentRound; k++) {             // up to no of buttons in current round
     console.log(k);
-    var delay = k * 2500; // calculates delay required to have the buttons play in sequence and not together.
-    setTimeout(playButton, delay, arrayToPlay[k], '1000'); // plays the current button in the array with the delay calculate
+    var delay = k * 2500;                                             // calculate delay required to have the buttons play in sequence and not together.
+    setTimeout(playButton, delay, arrayToPlay[k], "1000");            // plays the current button in the array with the delay calculated
   } /*for loop k*/
-  gameParameters.currentSequenceIndexRev = (gameParameters.currentRound - 1);
-  delay = gameParameters.currentRound * 2500;
-  setTimeout(showUserPrompt, delay, "Repeat");
-  setTimeout(function() { gameParameters.isWaitingForUser = true; }, delay)
-  cbUserSequence(arrayToPlay, cbplaySequence);
+  gameParameters.currentSequenceIndexRev = (gameParameters.currentRound - 1);  // reduce reverse pay sequence index by 1
+  delay = gameParameters.currentRound * 2500;                                   //calculate delay for next round to start
+  
+  if (gameParameters.playForward == true) {                                   // If playing in fwd direction
+    var prompt = "Repeat (as Seen)";                                          // Prompt user to copy sequence as is
+  }
+  else {                                                                      // if playig in reverse
+    var prompt = "Repeat (in Reverse)";;                                      // prompt user to repeat in reverse
+  }
+  
+  setTimeout(showUserPrompt, delay, prompt);                                  // prompt user to repeat as per direction selected
+  setTimeout(function() { gameParameters.isWaitingForUser = true; }, delay)   // prevent user clicking for duration of sequence
+  cbUserSequence(arrayToPlay, cbplaySequence);                                // at end of sequence read user responce again
 
 } /* playSequence function*/
 
-function startGame(Parameters) {
-  hideButtons();
-  showUserInfo(gameParameters.currentRound);
-  var noToLoad = 0;
-  // The following defines the array to play to the user 
-  var fwdPlayArray = [0, 1, 2, 3, 0, 1, 2, 3];
+function startGame(Parameters) {                                              
+  "use strict";
+  hideButtons();                                                              // during gameplay hide parameter buttons
+  showUserInfo(gameParameters.currentRound);                                  // display current rounf
+  var noToLoad = 0;                                                           // The following defines the array to play to the user
+  var PlayArray = [0, 1, 2, 3, 0, 1, 2, 3];
   var i = 0;
 
-  /* The following is a randomised array of numbers to play the buttons */
-  /*I am currently using a predicatable array so I know what is happening each time */
-
-  /*for (var i = Parameters.noOfRounds; i--;) {
-    noToLoad = Math.trunc(Math.random() * (Parameters.noOfButtons - 0.001));
-    fwdPlayArray[i] = noToLoad;
-    console.log(fwdPlayArray[i] + 'fwd' + i);
+  
+  /*for (var i = Parameters.noOfRounds; i--;) {                               // create array of button clicks for the number of rounds selected
+    noToLoad = Math.trunc(Math.random() * (Parameters.noOfButtons - 0.001));  // calculate random number from 0 to 3 to load into array for button click
+    PlayArray[i] = noToLoad;                                                   // store button click in array    
   }*/
-  playSequence(fwdPlayArray, userSequence, playSequence); // Uses the array defined to play the buttons for the user to copy
-  //userSequence(fwdPlayArray, playSequence); // Looks to read the response for the user
+  playSequence(PlayArray, userSequence, playSequence); // Uses the array defined to play the buttons for the user to copy
 }
--- js/ui-logic/ui.js
+++ js/ui-logic/ui.js
@@ -3,145 +3,147 @@
 ///  And show either 4 or 6 buttons as per options, also adjust button size to suit
 ///  The number of buttons on screen
 
-$(window).on('orientation resize load', function() {
-	configureButtons(gameParameters.noOfButtons);
+"use strict";
+
+$(window).on("orientation resize load", function() {			// on changes to screen reconfigure the buttons
+	configureButtons(gameParameters.noOfButtons);               // according to number and orientation of screen
 });
 
-function loserLoser(cbResetGame, buttonsInUse) {
-	var i = 0;
-	do {
-		var delay = i * 250;
-		setTimeout(playButton, delay, 0, '200');
+function loserLoser(cbResetGame, buttonsInUse) {				// on user selecting an incorrect button
+	var i = 0;				
+	do {														
+		var delay = i * 250;									// calculate delay for button to highlight each time
+		setTimeout(playButton, delay, 0, "200");				//  play red button repeatedly
 		i++;
 	} while (i < buttonsInUse);
-	var delayReset = delay * 2.8;
-	setTimeout(cbResetGame, delayReset);
+	var delayReset = delay * 2.8;								// calculate delay of restting game
+	setTimeout(cbResetGame, delayReset);						// reset game when sequence finishes
 }
 
 
-function winnerWinnerChickenDinner(cbResetGame, buttonsInUse) {
+function winnerWinnerChickenDinner(cbResetGame, buttonsInUse) {  // on user completeing game successfully
 	var i = 0;
-	do {
-		var delay = i * 250;
-		setTimeout(playButton, delay, i, '200');
+	do {	
+		var delay = i * 250;									// calculate delay for each button press
+		setTimeout(playButton, delay, i, "200");				// play buttons of winner sequence
 		i++;
-	} while (i < buttonsInUse);
-	var delayReset = delay * 2.8;
-	setTimeout(cbResetGame, delayReset);
+	} while (i < buttonsInUse);									// while less than buttons on screen
+	var delayReset = delay * 2.8;								// calculate delay for reset
+	setTimeout(cbResetGame, delayReset);						// reset game after winning sequence finishes
 }
 
 
 
 function configureButtons(buttonsInUse) {
-	var height = $(window).height();
-	var width = $(window).width();
-	if (buttonsInUse == 6) {
-		$('#red-button').css({ 'height': '15vh', 'width': '15vh' });
-		$('#yellow-button').css({ 'height': '15vh', 'width': '15vh' });
-		$('#green-button').css({ 'height': '15vh', 'width': '15vh' });
-		$('#blue-button').css({ 'height': '15vh', 'width': '15vh' });
-		if (width > height) {
-			$('#purple-button-portrait').removeClass('purple-button-style');
-			$('#brown-button-portrait').removeClass('brown-button-style');
-			$('#purple-button-landscape').addClass('purple-button-style').css({ 'height': '15vh', 'width': '15vh' });
-			$('#brown-button-landscape').addClass('brown-button-style').css({ 'height': '15vh', 'width': '15vh' });
+	var height = $(window).height();							// read screen height
+	var width = $(window).width();								// read screen width
+	if (buttonsInUse == 6) {												// if 6 buttons in use
+		$("#red-button").css({ "height": "15vh", "width": "15vh" });		// set button size to 15%
+		$("#yellow-button").css({ "height": "15vh", "width": "15vh" });		// of viewport height
+		$("#green-button").css({ "height": "15vh", "width": "15vh" });
+		$("#blue-button").css({ "height": "15vh", "width": "15vh" });
+		if (width > height) {														// if landscape screen
+			$("#purple-button-portrait").removeClass("purple-button-style");		// remove portrait purple/brown buttons
+			$("#brown-button-portrait").removeClass("brown-button-style");
+			$("#purple-button-landscape").addClass("purple-button-style").css({ "height": "15vh", "width": "15vh" });  // add extra buttons to landscape arrangement
+			$("#brown-button-landscape").addClass("brown-button-style").css({ "height": "15vh", "width": "15vh" });
 
 		}
-		else {
-			$('#purple-button-portrait').addClass('purple-button-style').css({ 'height': '15vh', 'width': '15vh' });
-			$('#brown-button-portrait').addClass('brown-button-style').css({ 'height': '15vh', 'width': '15vh' });
-			$('#purple-button-landscape').removeClass('purple-button-style');
-			$('#brown-button-landscape').removeClass('brown-button-style');
+		else {																												// if portrait sceen
+			$("#purple-button-portrait").addClass("purple-button-style").css({ "height": "15vh", "width": "15vh" });		// show portrait purple/brown buttons
+			$("#brown-button-portrait").addClass("brown-button-style").css({ "height": "15vh", "width": "15vh" });
+			$("#purple-button-landscape").removeClass("purple-button-style");												// hide landscapr purple/brown buttons
+			$("#brown-button-landscape").removeClass("brown-button-style");
 		}
 
 	}
-	else {
-		$('#purple-button-portrait').removeClass('purple-button-style');
-		$('#brown-button-portrait').removeClass('brown-button-style');
-		$('#purple-button-landscape').removeClass('purple-button-style');
-		$('#brown-button-landscape').removeClass('brown-button-style');
-		$('#red-button').css({ 'height': '20vh', 'width': '20vh' });
-		$('#yellow-button').css({ 'height': '20vh', 'width': '20vh' });
-		$('#green-button').css({ 'height': '20vh', 'width': '20vh' });
-		$('#blue-button').css({ 'height': '20vh', 'width': '20vh' });
+	else {																		// if 4 buttons selected
+		$("#purple-button-portrait").removeClass("purple-button-style");		// hide purple/brown buttons
+		$("#brown-button-portrait").removeClass("brown-button-style");
+		$("#purple-button-landscape").removeClass("purple-button-style");
+		$("#brown-button-landscape").removeClass("brown-button-style");
+		$("#red-button").css({ "height": "20vh", "width": "20vh" });			// set button size to 20% viewport height
+		$("#yellow-button").css({ "height": "20vh", "width": "20vh" });
+		$("#green-button").css({ "height": "20vh", "width": "20vh" });
+		$("#blue-button").css({ "height": "20vh", "width": "20vh" });
 	}
 }
 
-function labelRoundsButton(roundsLabel) {
-	$('#Rounds-Button').empty();
-	$('#Rounds-Button').append(roundsLabel + " Rounds");
+function labelRoundsButton(roundsLabel) {										// Label Rounds button when called
+	$("#Rounds-Button").empty();
+	$("#Rounds-Button").append(roundsLabel + " Rounds");
 }
 
-function labelDirectionButton(DirectionFwd) {
+function labelDirectionButton(DirectionFwd) {									// label direction button when called
 	var Direction;
-	if (DirectionFwd == true) {
-		Direction = "Fwd";
+	if (DirectionFwd == true) {													// if fwd play
+		Direction = "Fwd";														// set label to Fwd
 	}
-	else {
-		Direction = "Rev";
+	else {																		// if rev play
+		Direction = "Rev";														// set label to Rev
 	}
-	$('#Direction-Button').empty();
-	$('#Direction-Button').append("Direction: " + Direction);
+	$("#Direction-Button").empty();
+	$("#Direction-Button").append("Direction: " + Direction);					// give button label
 }
 
-function labelNoOfButtons(noOfButtons) {
-	$('#No-Of-Buttons').empty();
-	$('#No-Of-Buttons').append(noOfButtons + " Buttons");
+function labelNoOfButtons(noOfButtons) {										//  label no of buttons button
+	$("#No-Of-Buttons").empty();
+	$("#No-Of-Buttons").append(noOfButtons + " Buttons");						// give button label of buttons in use
 }
 
-function adjustDirection(playFwd) {
-	if (playFwd == true) {
+function adjustDirection(playFwd) {												// change direction when button clicked
+	if (playFwd == true) {														// if fwd change to reverse
 		gameParameters.playForward = false;
 	}
-	else if (playFwd == false) {
+	else if (playFwd == false) {												// if reverse change to fwd
 		gameParameters.playForward = true;
 	}
-	labelDirectionButton(gameParameters.playForward);
+	labelDirectionButton(gameParameters.playForward);							// label button with direction
 }
 
-function adjustNoOfButtons(buttonsUsed) {
-	if (buttonsUsed == 4) {
+function adjustNoOfButtons(buttonsUsed) {										// change number of buttons when clicked
+	if (buttonsUsed == 4) {													    // if currently 4, set to 6
 		gameParameters.noOfButtons = 6;
 	}
-	else if (buttonsUsed == 6) {
+	else if (buttonsUsed == 6) {												// if currently 6, set to 4
 		gameParameters.noOfButtons = 4;
 	}
-	labelNoOfButtons(gameParameters.noOfButtons);
-	configureButtons(gameParameters.noOfButtons);
+	labelNoOfButtons(gameParameters.noOfButtons);								// label button with no of buttons required
+	configureButtons(gameParameters.noOfButtons);								// show desired number of buttons on screen
 }
 
-function adjustDirection(FwdDirection) {
-	if (FwdDirection == true) {
+function adjustDirection(FwdDirection) {										// adjust direction of play when called
+	if (FwdDirection == true) {													// if fwd, on click set to reverse
 		gameParameters.playForward = false;
 	}
-	else {
+	else {																		//  if rev, on click set to fwd
 		gameParameters.playForward = true;
 	}
-	labelDirectionButton(gameParameters.playForward);
+	labelDirectionButton(gameParameters.playForward);							// label button according to direction selected.
 }
 
-function adjustRounds(roundsToPlay) {
-	if (roundsToPlay == 60) {
+function adjustRounds(roundsToPlay) {											// on click add 5 to number of rounds in play
+	if (roundsToPlay == 60) {													// max = 60 if click return to 5
 		gameParameters.noOfRounds = 5;
 	}
-	else {
+	else {																		// if less than 60, add 5 to number of buttons
 		gameParameters.noOfRounds += 5;
 	}
-	labelRoundsButton(gameParameters.noOfRounds);
+	labelRoundsButton(gameParameters.noOfRounds);								// label button with no of rounds to be played.
 }
 
-function hideButtons() {
-	$('#Rounds-Button').fadeOut(500);
-	$('#Direction-Button').fadeOut(500);
-	$('#No-Of-Buttons').fadeOut(500);
-	$('#start-button').fadeOut(500);
+function hideButtons() {														// on call (when game in play), hide parameter buttons
+	$("#Rounds-Button").fadeOut(500);
+	$("#Direction-Button").fadeOut(500);
+	$("#No-Of-Buttons").fadeOut(500);
+	$("#start-button").fadeOut(500);
 }
 
-function showUserInfo(Round) {
+function showUserInfo(Round) {													// on call, update current rounds being played onto screen
 	$("#options-panel").text("Round: " + Round);
 }
 
-function showUserPrompt(Prompt) {
+function showUserPrompt(Prompt) {												// on call, prompt user to watch or repeat, as given in call
 	$("#start-panel").text(Prompt);
 }
 
